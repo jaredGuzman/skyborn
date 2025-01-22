151ae0a0335cd71e8b923a0884c3fb9ef98ea37e
jaredGuzman <jarededwardguzman25@gmail.com>
2025-01-21 18:13:46 -0800

    Added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,14 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false,
+  }), compress(), svelte()],
   vite: {
     css: {
       preprocessorOptions: {
