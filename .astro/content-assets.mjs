5df5c9565f68e008d194cc82e6a491b09e93cad7
jaredGuzman <jarededwardguzman25@gmail.com>
2025-02-15 19:44:47 -0800

    Rectifying issues rendering blog content. Still need to resolve titles not showing in the blog post page correctly.

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1ssGG7 from "/public/image/uploads/pixel_cat_animation_upscaled.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-16-test.md";
+export default new Map([["/public/image/uploads/pixel_cat_animation_upscaled.gif?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025-02-16-test.md", __ASTRO_IMAGE_IMPORT_Z1ssGG7]]);
+		
